d4abaed49639df3730df513fbd5a02bbe9bbf05b
thaidui123 <vothongthai02012006@gmail.com>
2024-06-03 14:49:14 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace CSharp_HCN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.lblB = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtketqua = new System.Windows.Forms.TextBox();
+            this.lblketqua = new System.Windows.Forms.Label();
+            this.btncv = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.btndt = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbdientich = new System.Windows.Forms.RadioButton();
+            this.rdbchuvi = new System.Windows.Forms.RadioButton();
+            this.btnthoat1 = new System.Windows.Forms.Button();
+            this.btntinh = new System.Windows.Forms.Button();
+            this.lbltinh = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(122, 150);
+            this.lblA.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(20, 23);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "a";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(348, 159);
+            this.txtA.Margin = new System.Windows.Forms.Padding(5);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(164, 30);
+            this.txtA.TabIndex = 1;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(348, 248);
+            this.txtB.Margin = new System.Windows.Forms.Padding(5);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(164, 30);
+            this.txtB.TabIndex = 3;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblB.Location = new System.Drawing.Point(122, 239);
+            this.lblB.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(21, 23);
+            this.lblB.TabIndex = 2;
+            this.lblB.Text = "b";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(587, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hình Chữ Nhật";
+            // 
+            // txtketqua
+            // 
+            this.txtketqua.Location = new System.Drawing.Point(348, 341);
+            this.txtketqua.Margin = new System.Windows.Forms.Padding(5);
+            this.txtketqua.Name = "txtketqua";
+            this.txtketqua.Size = new System.Drawing.Size(164, 30);
+            this.txtketqua.TabIndex = 6;
+            // 
+            // lblketqua
+            // 
+            this.lblketqua.AutoSize = true;
+            this.lblketqua.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblketqua.Location = new System.Drawing.Point(122, 333);
+            this.lblketqua.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblketqua.Name = "lblketqua";
+            this.lblketqua.Size = new System.Drawing.Size(77, 23);
+            this.lblketqua.TabIndex = 5;
+            this.lblketqua.Text = "Kết Quả";
+            // 
+            // btncv
+            // 
+            this.btncv.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncv.Location = new System.Drawing.Point(92, 430);
+            this.btncv.Margin = new System.Windows.Forms.Padding(5);
+            this.btncv.Name = "btncv";
+            this.btncv.Size = new System.Drawing.Size(162, 60);
+            this.btncv.TabIndex = 7;
+            this.btncv.Text = "Chu vi";
+            this.btncv.UseVisualStyleBackColor = true;
+            this.btncv.Click += new System.EventHandler(this.btncv_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthoat.Location = new System.Drawing.Point(1142, 701);
+            this.btnthoat.Margin = new System.Windows.Forms.Padding(5);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(125, 41);
+            this.btnthoat.TabIndex = 9;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            this.btnthoat.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnthoat_MouseClick);
+            // 
+            // btndt
+            // 
+            this.btndt.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndt.Location = new System.Drawing.Point(302, 430);
+            this.btndt.Margin = new System.Windows.Forms.Padding(5);
+            this.btndt.Name = "btndt";
+            this.btndt.Size = new System.Drawing.Size(162, 60);
+            this.btndt.TabIndex = 10;
+            this.btndt.Text = "Diện tích";
+            this.btndt.UseVisualStyleBackColor = true;
+            this.btndt.Click += new System.EventHandler(this.btndt_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbltinh);
+            this.groupBox1.Controls.Add(this.rdbdientich);
+            this.groupBox1.Controls.Add(this.rdbchuvi);
+            this.groupBox1.Location = new System.Drawing.Point(592, 159);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(294, 178);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn";
+            // 
+            // rdbdientich
+            // 
+            this.rdbdientich.AutoSize = true;
+            this.rdbdientich.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbdientich.Location = new System.Drawing.Point(48, 75);
+            this.rdbdientich.Margin = new System.Windows.Forms.Padding(5);
+            this.rdbdientich.Name = "rdbdientich";
+            this.rdbdientich.Size = new System.Drawing.Size(66, 17);
+            this.rdbdientich.TabIndex = 0;
+            this.rdbdientich.TabStop = true;
+            this.rdbdientich.Text = "Diện tích";
+            this.rdbdientich.UseVisualStyleBackColor = true;
+            this.rdbdientich.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rdbchuvi
+            // 
+            this.rdbchuvi.AutoSize = true;
+            this.rdbchuvi.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbchuvi.Location = new System.Drawing.Point(48, 35);
+            this.rdbchuvi.Margin = new System.Windows.Forms.Padding(5);
+            this.rdbchuvi.Name = "rdbchuvi";
+            this.rdbchuvi.Size = new System.Drawing.Size(55, 17);
+            this.rdbchuvi.TabIndex = 0;
+            this.rdbchuvi.TabStop = true;
+            this.rdbchuvi.Text = "Chu vi";
+            this.rdbchuvi.UseVisualStyleBackColor = true;
+            this.rdbchuvi.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // btnthoat1
+            // 
+            this.btnthoat1.Location = new System.Drawing.Point(373, 516);
+            this.btnthoat1.Name = "btnthoat1";
+            this.btnthoat1.Size = new System.Drawing.Size(123, 35);
+            this.btnthoat1.TabIndex = 12;
+            this.btnthoat1.Text = "Thoát";
+            this.btnthoat1.UseVisualStyleBackColor = true;
+            this.btnthoat1.Click += new System.EventHandler(this.btnthoat1_Click);
+            // 
+            // btntinh
+            // 
+            this.btntinh.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntinh.Location = new System.Drawing.Point(686, 370);
+            this.btntinh.Margin = new System.Windows.Forms.Padding(5);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(103, 49);
+            this.btntinh.TabIndex = 11;
+            this.btntinh.Text = "Tính";
+            this.btntinh.UseVisualStyleBackColor = true;
+            this.btntinh.Click += new System.EventHandler(this.btntinh_Click);
+            // 
+            // lbltinh
+            // 
+            this.lbltinh.AutoSize = true;
+            this.lbltinh.Location = new System.Drawing.Point(99, 132);
+            this.lbltinh.Name = "lbltinh";
+            this.lbltinh.Size = new System.Drawing.Size(47, 23);
+            this.lbltinh.TabIndex = 1;
+            this.lbltinh.Text = "Tính";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.ClientSize = new System.Drawing.Size(1008, 618);
+            this.Controls.Add(this.btntinh);
+            this.Controls.Add(this.btnthoat1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btndt);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btncv);
+            this.Controls.Add(this.txtketqua);
+            this.Controls.Add(this.lblketqua);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblA);
+            this.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtketqua;
+        private System.Windows.Forms.Label lblketqua;
+        private System.Windows.Forms.Button btncv;
+        private System.Windows.Forms.Button btnthoat;
+        private System.Windows.Forms.Button btndt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbchuvi;
+        private System.Windows.Forms.RadioButton rdbdientich;
+        private System.Windows.Forms.Button btnthoat1;
+        private System.Windows.Forms.Button btntinh;
+        private System.Windows.Forms.Label lbltinh;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_HCN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncv_Click(object sender, EventArgs e)
+        {
+            if (txtA.Text == "" || txtB.Text == "")
+            {
+                MessageBox.Show("Phải nhập chiều ngang và dọc");
+                return;
+            }
+            Double a, b, S, P;
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+            P = 2 * (a + b);
+            txtketqua.Text=P.ToString();
+
+        }
+
+        private void btndt_Click(object sender, EventArgs e)
+        {
+            if (txtA.Text == "" || txtB.Text == "")
+            {
+                MessageBox.Show("Phải nhập chiều ngang và dọc");
+                return;
+            }
+            Double a, b, S, P;
+            a = Convert.ToDouble(txtA.Text);
+            b = Convert.ToDouble(txtB.Text);
+            S = a * b;
+            txtketqua.Text = S.ToString();
+
+        }
+
+        private void btnthoat_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnthoat1_Click(object sender, EventArgs e)
+        {
+            this.Close ();  
+        }
+
+        private void btntinh_Click(object sender, EventArgs e)
+        {
+            if(rdbchuvi.Checked)
+            {
+                lbltinh.Text = "Chu vi";
+                if (txtA.Text == "" || txtB.Text == "")
+                {
+                    MessageBox.Show("Phải nhập chiều ngang và dọc");
+                    return;
+                }
+                Double a, b, S, P;
+                a = Convert.ToDouble(txtA.Text);
+                b = Convert.ToDouble(txtB.Text);
+                P = 2 * (a + b);
+                txtketqua.Text = P.ToString();
+
+            }
+            if (rdbdientich.Checked)
+            {
+                lbltinh.Text = "Diện tích";
+                if (txtA.Text == "" || txtB.Text == "")
+                {
+                    MessageBox.Show("Phải nhập chiều ngang và dọc");
+                    return;
+                }
+                Double a, b, S, P;
+                a = Convert.ToDouble(txtA.Text);
+                b = Convert.ToDouble(txtB.Text);
+                S = a * b;
+                txtketqua.Text = S.ToString();
+            }
+        }
+    }
+}
